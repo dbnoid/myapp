94dac820c020ae48b64855a1f318559dd94f4880
정창인 <dbnoid@gmail.com>
2022-10-13 17:22:56 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['172.17.74.236']
-STATIC_ROOT = BASE_DIR / 'static/'
+ALLOWED_HOSTS = ['172.17.74.236', 'localhost']
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = []
 DEBUG = False
