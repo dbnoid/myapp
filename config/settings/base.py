7023e65e43e4d0449d36e1f79e41850e949cf051
정창인 <dbnoid@gmail.com>
2022-10-13 19:30:35 +0900

    Setting변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -201,11 +201,26 @@ LOGGING = {
 }
 
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://172.17.74.236",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000"
-]
-
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
